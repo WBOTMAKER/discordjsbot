7e938e8950d6de5352aad8f62975b12f8f2f27fc
STAYCUTE <34066247+WBOTMAKER@users.noreply.github.com>
2019-07-15 21:33:20 +0900

    Create 핑.js

---
+++ 명령어파일/핑.js
@@ -0,0 +1,9 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (client, message, args) => {
+  try{
+message.channel.send(`퐁!\n봇의 API 핑은${client.ping}입니다!`)
+  }catch(e) {
+    message.channel.send(`에러발생 \n\n \`\`\`js\n${e}\n\`\`\`\n\n`); 
+  }
+}
