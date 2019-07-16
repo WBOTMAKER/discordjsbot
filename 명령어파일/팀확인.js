7d42ed7e23b32d8307d35e60bf2333483a0079a8
STAYCUTE <34066247+WBOTMAKER@users.noreply.github.com>
2019-07-16 19:16:12 +0900

    Update 팀확인.js

--- 명령어파일/팀확인.js
+++ 명령어파일/팀확인.js
@@ -4,9 +4,9 @@ var config = require("../setting.json")
 module.exports.run = async (client, message, args) => {
   try{
 if (config.manager.includes(message.author.id)){
-  message.reply("반갑습니다 팀원님!")
+  message.reply("당신은 저의 팀원입니다.")
 }else{
-  message.reply("당신은 봇의 팀이 아닙니다!")
+  message.reply("당신은 저의 팀원이 아닙니다!")
 }
   }catch(e) {
     message.channel.send(`에러발생 \n\n \`\`\`js\n${e}\n\`\`\`\n\n`);
